2fa98450a4dc5a081dcc2405efbdab4b7d08fe72
zyqaq412 <2791517764@qq.com>
2023-11-13 16:50:48 +0800

    配置修改

--- zymall-coupon/src/main/java/com/hzy/zymall/coupon/ZymallCouponApplication.java
+++ zymall-coupon/src/main/java/com/hzy/zymall/coupon/ZymallCouponApplication.java
@@ -1,9 +1,11 @@
 package com.hzy.zymall.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hzy.zymall.coupon.dao")
 public class ZymallCouponApplication {
 
     public static void main(String[] args) {
--- zymall-product/src/main/java/com/hzy/zymall/product/ZymallProductApplication.java
+++ zymall-product/src/main/java/com/hzy/zymall/product/ZymallProductApplication.java
@@ -1,9 +1,11 @@
 package com.hzy.zymall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hzy.zymall.product.dao")
 public class ZymallProductApplication {
 
     public static void main(String[] args) {
