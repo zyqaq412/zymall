c5f6e85d610feff1641006f1fbe7578461b94c72
zyqaq412 <2791517764@qq.com>
2023-11-14 15:38:16 +0800

    添加zymall-gateway网关

---
+++ zymall-gateway/src/main/java/com/hzy/gateway/ZymallGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.hzy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient // 开启服务注册发现
+public class ZymallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZymallGatewayApplication.class, args);
+    }
+
+}
